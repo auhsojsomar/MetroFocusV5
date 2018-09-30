422bb954c0c44b1c904151cde53ad9ffec6339ba
auhsojsomar <auhsojsomar@gmail.com>
2018-09-30 22:33:28 +0800

    commit

--- Admin/php/parts/partsdefect.php
+++ Admin/php/parts/partsdefect.php
@@ -1,11 +1,13 @@
 <?php
     include '../../../User/includes/db.php';
-    $id = $_POST['uid'];
+    $id = $_POST['user_id3'];
     $user = $_COOKIE['email'];
     $desc = $_POST['problem'];
+    $quan = $_POST['defect'];
     date_default_timezone_set('Asia/Manila');
     $date = date('n/j/Y g:i A');
-    $sql = mysqli_query($con,"SELECT * FROM activitylogs WHERE id = $id");
+    $sql = mysqli_query($con,"SELECT * FROM parts WHERE id = $id");
     $row = mysqli_fetch_array($sql);
-    mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$row[0]','Defect','$date','Parts','$user','$desc')");
+    mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$row[2]','Defect','$date','Parts','$user','$desc')");
+    mysqli_query($con,"UPDATE parts SET quantity = quantity - $quan");
 ?>
