8079e002492330333298e2bc9f4811e5d55c3a8d
auhsojsomar <auhsojsomar@gmail.com>
2018-10-01 01:50:43 +0800

    fix load

--- Admin/php/load/accessoriesquantityadd.php
+++ Admin/php/load/accessoriesquantityadd.php
@@ -3,4 +3,10 @@
 	$quantity = $_POST['loadtext'];
 	$id = $_POST['user_id2'];
 	$query = mysqli_query($con,"UPDATE accessories SET quantity = quantity + $quantity WHERE id = $id");
+	$sql = mysqli_query($con,"SELECT name FROM accessories WHERE id = $id");
+	$name = mysqli_fetch_array($sql);
+	$user = $_COOKIE['email'];
+	date_default_timezone_set('Asia/Manila');
+	$date = date('n/j/Y g:i A');
+	mysqli_query($con,"INSERT INTO activitylogs (name,action,quantity,datemod,type,user)VALUES('$name[0]','Add Stocks',$quantity,'$date','Accessories','$user')");
 ?>
