74f643fb88e41372c4ed4d2762d5788fe5719606
auhsojsomar <auhsojsomar@gmail.com>
2018-09-30 19:45:40 +0800

    commit

--- Admin/php/parts/partsdefect.php
+++ Admin/php/parts/partsdefect.php
@@ -1,5 +1,10 @@
 <?php
     include '../../../User/includes/db.php';
     $id = $_POST['uid'];
-    mysqli_query($con,"INSERT INTO activitylogs ");
+    $user = $_COOKIE['email'];
+    date_default_timezone_set('Asia/Manila');
+    $date = date('n/j/Y g:i A');
+    $sql = mysqli_query($con,"SELECT * FROM activitylogs WHERE id = $id");
+    $row = mysqli_fetch_array($sql);
+    mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$row[0]','Defect','$date','Parts','$user','')");
 ?>
