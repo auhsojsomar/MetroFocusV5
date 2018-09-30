0d6ffb75d513d724c5b99dc010712528e0897f4c
auhsojsomar <auhsojsomar@gmail.com>
2018-09-30 23:32:49 +0800

    commit

--- Admin/php/admin/admindelete.php
+++ Admin/php/admin/admindelete.php
@@ -9,14 +9,16 @@ if(isset($_POST["user_id"]))
 		echo "Error";
 	}
 	else{
-		mysqli_query($con,"UPDATE loginform SET deleted = 1 WHERE id = $id");
-		echo "Data Deleted";
+		if(mysqli_query($con,"UPDATE loginform SET deleted = 1 WHERE id = $id")){
+			$user = $_COOKIE['email'];
+			$sql = mysqli_query($con,"SELECT username FROM loginform WHERE id = $id");
+			$name = mysqli_fetch_array($sql);
+			date_default_timezone_set('Asia/Manila');
+			$date = date('n/j/Y g:i A');
+			mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$name[0]','Deleted','$date','Admin','$user')");
+			echo "Data Deleted";
+		}
 	}
 }
-$user = $_COOKIE['email'];
-$sql = mysqli_query($con,"SELECT username FROM loginform WHERE id = $id");
-$name = mysqli_fetch_array($sql);
-date_default_timezone_set('Asia/Manila');
-$date = date('n/j/Y g:i A');
-mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$name[0]','Deleted','$date','Admin','$user')");
+	
 ?>
