febe7001925e83c3d497139ca32572ef8b6a24e3
auhsojsomar <auhsojsomar@gmail.com>
2018-10-02 09:51:21 +0800

    commit

---
+++ Admin/php/reservation/accessoriesmessage.php
@@ -0,0 +1,101 @@
+<?php 
+    include '../../../User/includes/db.php';
+    $id = $_POST['id'];
+    $sql = mysqli_query($con,"SELECT loginform.firstname,accessories.name,accessories.price,reservation.reservationdate,loginform.cnumber FROM loginform,reservation,accessories WHERE accessories.id = reservation.itemid AND reservation.username = loginform.username AND reservation.id = $id AND reservation.status = 'Pending'");
+    $row = mysqli_fetch_array($sql);
+    $firstname = $row[0];
+    $name = $row[1];
+    $price = $row[2];
+    $datetime = $row[3];
+    $cnumber = $row[4];
+    $date = date("F d Y",strtotime($datetime));
+    $time = date("g:i A",strtotime($datetime));
+    $message =  "Hi $firstname! This message is the confirmation that your item which is $name is already reserved for you and costs ₱".number_format($price,2,'.',',')." .We remind that you will get the item on $date at $time \n\n --Metro Focus--";
+    
+    // itexmo.com
+    // function itexmo($number,$message,$apicode){
+    // $url = 'https://www.itexmo.com/php_api/api.php';
+    // $itexmo = array('1' => $number, '2' => $message, '3' => $apicode);
+    // $param = array(
+    // 'http' => array(
+    //     'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+    //     'method'  => 'POST',
+    //     'content' => http_build_query($itexmo),
+    // ),
+    // );
+    // $context  = stream_context_create($param);
+    // return file_get_contents($url, false, $context);}
+    // $result = itexmo($cnumber,$message,"TR-METRO406141_A22UN");
+    // if ($result == ""){
+    // echo "iTexMo: No response from server!!!
+    // Please check the METHOD used (CURL or CURL-LESS). If you are using CURL then try CURL-LESS and vice versa.  
+    // Please CONTACT US for help. ";  
+    // }else if ($result == 0){
+    // echo "Confirmation Sent!";
+    // }
+    // else{   
+    // echo "Error Num ". $result . " was encountered!";
+    // }
+
+    // https://semaphore.co/docs
+
+    // $ch = curl_init();
+    // $parameters = array(
+    //     'apikey' => '54b32b387e1973390e6a5617c2e54f38', //Your API KEY
+    //     'number' => $cnumber,
+    //     'message' => $message,
+    //     'sendername' => 'SEMAPHORE'
+    // );
+    // curl_setopt( $ch, CURLOPT_URL,'http://api.semaphore.co/api/v4/messages' );
+    // curl_setopt( $ch, CURLOPT_POST, 1 );
+
+    // //Send the parameters set above with the request
+    // curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
+
+    // // Receive response from server
+    // curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+    // $output = curl_exec( $ch );
+    // curl_close ($ch);
+
+    // //Show the server response
+    // //echo $output;
+    // echo "Confirmation Sent!";
+
+    // https://smsgateway.me/
+
+    require '../sms/autoload.php';
+
+    use SMSGatewayMe\Client\ApiClient;
+    use SMSGatewayMe\Client\Configuration;
+    use SMSGatewayMe\Client\Api\MessageApi;
+    use SMSGatewayMe\Client\Model\SendMessageRequest;
+
+    if(!empty($cnumber)){
+        // Configure client
+        $config = Configuration::getDefaultConfiguration();
+        $config->setApiKey('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImlhdCI6MTUzNzk2MjE5MCwiZXhwIjo0MTAyNDQ0ODAwLCJ1aWQiOjYxNzUxLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.fZ11G2HZ6QvIh2WjaGH63XsWyIBjbRItuuNwIPCfGiU');
+        $apiClient = new ApiClient($config);
+        $messageClient = new MessageApi($apiClient);
+
+        // Sending a SMS Message
+        $sendMessageRequest1 = new SendMessageRequest([
+            'phoneNumber' => $cnumber,
+            'message' => $message,
+            'deviceId' => 102592
+        ]);
+        // $sendMessageRequest2 = new SendMessageRequest([
+        //     'phoneNumber' => '07791064781',
+        //     'message' => 'test2',
+        //     'deviceId' => 2
+        // ]);
+        $sendMessages = $messageClient->sendMessages([
+            $sendMessageRequest1
+            // $sendMessageRequest2
+        ]);
+        // print_r($sendMessages);
+        echo "Message Sent!";
+    }
+    else{
+        echo "Error";
+    }
+ ?>
--- Admin/reservation.php
+++ Admin/reservation.php
@@ -376,21 +376,28 @@ if($_COOKIE['role'] == 'Admin'){
             $(document).on('click','button[name="confirm2"]',function(){
                 var user_id2 = $(this).attr("id");
                 $.ajax({
-                     url:"php/reservation/reservationconfirm.php",
-                        method:"POST",
-                        data:{user_id2:user_id2},
-                        success:function(data)
-                        {
-                            swal({
-                                title: "Confirmed",
-                                text: "",
-                                icon: "success",
-                                closeOnClickOutside: false,
-                            })
-                            .then((value) => {
-                            dataTable2.ajax.reload();
-                        });
+                    url:'php/reservation/accessoriesmessage.php',
+                    method:'POST',
+                    data:{id:user_id2},
+                    success:function(data){
+                        if(data == "Error"){
+                            swal('Error','','error',{
+                                closeOnClickOutside:false
+                            });
                         }
+                        else{
+                            $.ajax({
+                                url:'php/reservation/reservationconfirm.php',
+                                method:'POST',
+                                data:{id:user_id2},
+                                success:function(data){
+                                    swal('Message Sent!','','success',{
+                                        closeOnClickOutside:false
+                                    })
+                                }
+                            });
+                        }
+                    }
                 });
             });
             document.addEventListener('DOMContentLoaded',function(){
