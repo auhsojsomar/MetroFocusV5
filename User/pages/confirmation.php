7e330d40238022a28b6ea4384c0351ef19275f7e
auhsojsomar <auhsojsomar@gmail.com>
2018-10-05 14:29:31 +0800

    commit

--- User/pages/confirmation.php
+++ User/pages/confirmation.php
@@ -171,9 +171,6 @@
               $('#verify').removeClass('is-danger');
               $('#verifyicon').removeClass('fal fa-exclamation-triangle');
               $('#verifymessage').html('');
-              <?php 
-              
-              ?>
             }
             return false;
           }
--- User/php/confirmation.php
+++ User/php/confirmation.php
@@ -1,4 +1,6 @@
 <?php
     include '../includes/db.php';
-    
+    $user = $_SESSION['NotVerify'];
+    $code = $_POST['code'];
+    $sql = mysqli_query($con,"SELECT verification FROM loginform WHERE username = '$user'");
 ?>
