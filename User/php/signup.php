e8202bb595022fa974806ca0f49b8228d93e5e67
auhsojsomar <auhsojsomar@gmail.com>
2018-10-05 18:16:26 +0800

    commit

--- User/php/signup.php
+++ User/php/signup.php
@@ -12,4 +12,25 @@
 		$_SESSION['user'] = $email;
 		echo "Success";
 	}
+	require '../../Admin/php/sms/autoload.php';
+        
+    use SMSGatewayMe\Client\ApiClient;
+    use SMSGatewayMe\Client\Configuration;
+    use SMSGatewayMe\Client\Api\MessageApi;
+    use SMSGatewayMe\Client\Model\SendMessageRequest;
+
+    $config = Configuration::getDefaultConfiguration();
+        $config->setApiKey('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhZG1pbiIsImlhdCI6MTUzNzk2MjE5MCwiZXhwIjo0MTAyNDQ0ODAwLCJ1aWQiOjYxNzUxLCJyb2xlcyI6WyJST0xFX1VTRVIiXX0.fZ11G2HZ6QvIh2WjaGH63XsWyIBjbRItuuNwIPCfGiU');
+        $apiClient = new ApiClient($config);
+        $messageClient = new MessageApi($apiClient);
+
+        $sendMessageRequest1 = new SendMessageRequest([
+            'phoneNumber' => $cnum,
+            'message' => 'Your verification code is:'.$rand,
+            'deviceId' => 102592
+        ]);
+        $sendMessages = $messageClient->sendMessages([
+            $sendMessageRequest1
+        ]);
+        echo "Message Sent!";
  ?>
