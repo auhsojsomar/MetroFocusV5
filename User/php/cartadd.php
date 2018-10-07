e9dfee40f833d419fe1219fbf583533c62f53f09
auhsojsomar <auhsojsomar@gmail.com>
2018-10-07 20:25:26 +0800

    fix cart

--- User/php/cartadd.php
+++ User/php/cartadd.php
@@ -11,5 +11,11 @@
 	for ($i=0; $i < $num; $i++) { 
 		mysqli_query($con,"INSERT INTO reservation (username,reservationdate,itemid,category,itemquantity)VALUES('$email','$d',$itemid[$i],'$category[$i]',$quan[$i])");
 		mysqli_query($con,"DELETE FROM cart WHERE user = '$email'");
+		if($category[$i] == 'Parts'){
+			mysqli_query($con,"UPDATE parts SET quantity = quantity - $quan[$i] WHERE id = $itemid[$i]");
+		}
+		if($category[$i] == 'Accessories'){
+			mysqli_query($con,"UPDATE accessories SET quantity = quantity - $quan[$i] WHERE id = $itemid[$i]");
+		}
 	}
  ?>
