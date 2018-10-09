68078cd7f2c00ca19d44552980e80b198817c378
auhsojsomar <auhsojsomar@gmail.com>
2018-10-10 01:42:25 +0800

    fix button in reservation

--- Admin/php/reservation/accessoriesfetch.php
+++ Admin/php/reservation/accessoriesfetch.php
@@ -10,7 +10,7 @@ while($row = mysqli_fetch_array($sql)){
         $button = '<button style="width:20px;" type="button" name="reject" id="'.$row["id"].'" class="button is-danger is-small tooltip" data-tooltip="Reject"><i class="fal fa-times"></i></button><button style="width:20px;" type="button" name="done" id="'.$row["id"].'" class="button is-success is-small tooltip" data-tooltip="Done"><i class="fal fa-check"></i></button>';
     }
     else{
-        $button = '<button style="width:20px;" type="button" name="confirm2" id="'.$row["id"].'" class="button is-link is-small tooltip" data-tooltip="Confirm"><i class="fal fa-file-check"></i></button><button style="width:20px;" type="button" name="reject" id="'.$row["id"].'" class="button is-danger is-small tooltip" data-tooltip="Reject"><i class="fal fa-times"></i></button>';
+        $button = '<button style="width:20px;" type="button" name="confirm2" id="'.$row["id"].'" class="button is-success is-small tooltip" data-tooltip="Confirm"><i class="fal fa-check"></i></button><button style="width:20px;" type="button" name="reject" id="'.$row["id"].'" class="button is-danger is-small tooltip" data-tooltip="Reject"><i class="fal fa-times"></i></button>';
     }
     $output['data'][] = array(
         $row['username'],
