e5396eee54f41ba864d3f6d5e5c7a0cd46f65d77
auhsojsomar <auhsojsomar@gmail.com>
2018-10-11 02:59:02 +0800

    add birth day

--- User/php/signup.php
+++ User/php/signup.php
@@ -4,12 +4,13 @@
 	$date = date('F d, Y');
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
+	$bday = $_POST['bday'];
 	$email = $_POST['email'];
 	$cn = $_POST['cnum'];
 	$password = md5($_POST['password']);
 	$rand = rand(100000,999999);
 	$_SESSION['user'] = $email;
-	if($sql = mysqli_query($con,"INSERT INTO loginform (firstname,lastname,username,password,cnumber,created,verification)VALUES('$fname','$lname','$email','$password','$cn','$date','$rand')")){
+	if($sql = mysqli_query($con,"INSERT INTO loginform (firstname,lastname,username,password,cnumber,created,verification,bday)VALUES('$fname','$lname','$email','$password','$cn','$date','$rand','$bday')")){
 		$_SESSION['user'] = $email;
 		include_once 'code.php';
 		echo "Success";
