b0b290e4b31207caa98bd2a6f957f1694a824ad0
auhsojsomar <auhsojsomar@gmail.com>
2018-10-12 02:27:07 +0800

    commit

--- Admin/php/admin/admindelete.php
+++ Admin/php/admin/admindelete.php
@@ -3,18 +3,18 @@ include '../../../User/includes/db.php';
 if(isset($_POST["user_id"]))
 {
 	$id = $_POST['user_id'];
+	date_default_timezone_set('Asia/Manila');
+	$date = date('n/j/Y g:i A');
 	$query = mysqli_query($con,"SELECT * FROM loginform WHERE status = 'Admin' AND deleted = 0");
 	$row = mysqli_num_rows($query);
 	if($row <= 1){
 		echo "Error";
 	}
 	else{
-		if(mysqli_query($con,"UPDATE loginform SET deleted = 1 WHERE id = $id")){
+		if(mysqli_query($con,"UPDATE loginform SET deleted = 1, date_deleted = '$date' WHERE id = $id")){
 			$user = $_COOKIE['email'];
 			$sql = mysqli_query($con,"SELECT username FROM loginform WHERE id = $id");
 			$name = mysqli_fetch_array($sql);
-			date_default_timezone_set('Asia/Manila');
-			$date = date('n/j/Y g:i A');
 			mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$name[0]','Deleted','$date','Admin','$user')");
 			echo "Data Deleted";
 		}
--- Admin/php/faq/faqdelete.php
+++ Admin/php/faq/faqdelete.php
@@ -1,7 +1,9 @@
 <?php 
 	include '../../../User/includes/db.php';
 	$id = $_POST['id'];
-	if(mysqli_query($con,"UPDATE faq SET deleted = 1 WHERE id = $id")){
+	date_default_timezone_set('Asia/Manila');
+	$date = date('n/j/Y g:i A');
+	if(mysqli_query($con,"UPDATE faq SET deleted = 1, date_deleted = '$date' WHERE id = $id")){
 		echo "Success";
 	}
 	else{
@@ -10,7 +12,5 @@
 	$user = $_COOKIE['email'];
 	$sql = mysqli_query($con,"SELECT question FROM faq WHERE id = $id");
 	$question = mysqli_fetch_array($sql);
-	date_default_timezone_set('Asia/Manila');
-	$date = date('n/j/Y g:i A');
 	mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$question[0]','Deleted','$date','FAQ','$user')");
  ?>
