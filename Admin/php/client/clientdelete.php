ceb6cb4f2412822e5d6e061bb8cb58378b9611d8
auhsojsomar <auhsojsomar@gmail.com>
2018-10-13 04:42:04 +0800

    add checkbox

--- Admin/php/client/clientdelete.php
+++ Admin/php/client/clientdelete.php
@@ -1,16 +1,26 @@
 <?php 
 include '../../../User/includes/db.php';
-if(isset($_POST["user_id"]))
-{
-	$id = $_POST['user_id'];
-	date_default_timezone_set('Asia/Manila');
-	$date = date('n/j/Y g:i A');
-	if(mysqli_query($con,"UPDATE loginform SET deleted = 1, date_deleted = '$date' WHERE id = $id")){
-		echo "Data Deleted";
+$id = $_POST['reasonid'];
+$description = $_POST['reason'];
+$action = $_POST['reasonaction'];
+date_default_timezone_set('Asia/Manila');
+$date = date('n/j/Y g:i A');
+$user = $_COOKIE['email'];
+if($action == 'Delete'){
+	$sql = mysqli_query($con,"SELECT username FROM loginform WHERE id = $id");
+	$name = mysqli_fetch_array($sql);
+	mysqli_query($con,"UPDATE loginform SET deleted = 1, date_deleted = '$date' WHERE id = $id");
+	mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$name[0]','Deleted','$date','User','$user','$description')");
+}
+else if($action == 'DeleteAll'){
+	$array = explode(',',$id);
+	foreach($array as $newid){
+		$sql = mysqli_query($con,"SELECT username FROM loginform WHERE id = $newid");
+		$name = mysqli_fetch_array($sql);
+		mysqli_query($con,"UPDATE loginform SET deleted = 1, date_deleted = '$date' WHERE id = $newid");
+		if($name != ''){
+			mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$name[0]','Deleted','$date','User','$user','$description')");
+		}
 	}
 }
-$user = $_COOKIE['email'];
-$sql = mysqli_query($con,"SELECT username FROM loginform WHERE id = $id");
-$name = mysqli_fetch_array($sql);
-mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$name[0]','Deleted','$date','User','$user')");
 ?>
