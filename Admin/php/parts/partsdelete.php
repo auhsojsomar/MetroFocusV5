c9282dee7516faaa329d49e8ac68eaf321101b80
auhsojsomar <auhsojsomar@gmail.com>
2018-10-13 01:56:37 +0800

    commit

--- Admin/php/parts/partsdelete.php
+++ Admin/php/parts/partsdelete.php
@@ -6,19 +6,19 @@ $action = $_POST['reasonaction'];
 date_default_timezone_set('Asia/Manila');
 $date = date('n/j/Y g:i A');
 $user = $_COOKIE['email'];
-if($description == 'Delete'){
+if($action == 'Delete'){
 	$sql = mysqli_query($con,"SELECT name FROM parts WHERE id = $id");
 	$name = mysqli_fetch_array($sql);
 	mysqli_query($con,"UPDATE parts SET deleted = 1, date_deleted = '$date' WHERE id = $id");
-	mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$name[0]','Deleted','$date','Parts','$user')");
+	mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$name[0]','Deleted','$date','Parts','$user','$description')");
 }
-else if($description == 'DeleteAll'){
+else if($action == 'DeleteAll'){
 	$array = explode(',',$id);
 	foreach($array as $newid){
 		$sql = mysqli_query($con,"SELECT name FROM parts WHERE id = $newid");
 		$name = mysqli_fetch_array($sql);
-		mysqli_query($con,"UPDATE parts SET deleted = 1, date_deleted = '$date' WHERE id = $id");
-		mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$name[0]','Deleted','$date','Parts','$user')");
+		mysqli_query($con,"UPDATE parts SET deleted = 1, date_deleted = '$date' WHERE id = $newid");
+		mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$name[0]','Deleted','$date','Parts','$user','$description')");
 	}
 }
 ?>
