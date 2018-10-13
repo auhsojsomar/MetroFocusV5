22e6d69d780ab476847f94dc4dc0aa55b4e157e3
auhsojsomar <auhsojsomar@gmail.com>
2018-10-13 12:26:24 +0800

    commit

--- Admin/php/trash/delete.php
+++ Admin/php/trash/delete.php
@@ -84,4 +84,23 @@
             }
         }
     }
+    else if($type == 'Admin'){
+        if($action == 'Delete'){
+            $sql = mysqli_query($con,"SELECT username FROM loginform WHERE status='Admin' AND id = $id");
+            $row = mysqli_fetch_array($sql);
+            mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$row[0]','Permanent Delete','$date','Admin','$user','$description')");
+            mysqli_query($con,"DELETE FROM loginform WHERE id = $id");
+        }
+        else if($action == 'DeleteAll'){
+            $array = explode(',',$id);
+            foreach($array as $newid){
+                $sql = mysqli_query($con,"SELECT username FROM loginform WHERE status='Admin' AND id = $newid");
+                $row = mysqli_fetch_array($sql);
+                if($row[0] != ''){
+                    mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user,description)VALUES('$row[0]','Permanent Delete','$date','Admin','$user','$description')");
+                }
+                mysqli_query($con,"DELETE FROM loginform WHERE id = $newid");
+            }
+        }
+    }
 ?>
--- Admin/php/trash/restore.php
+++ Admin/php/trash/restore.php
@@ -79,4 +79,22 @@
             }
         }
     }
+    else if($type == 'Admin'){
+        if($action == 'Restore'){
+            $sql = mysqli_query($con,"SELECT username FROM admin WHERE status='Admin' AND id = $id");
+            $row = mysqli_fetch_array($sql);
+            mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$row[0]','Recover','$date','Admin','$user')");
+            mysqli_query($con,"UPDATE loginform SET deleted = 0 WHERE id = $id");
+        }
+        else if($action == 'RestoreAll'){
+            foreach($id as $newid){
+                $sql = mysqli_query($con,"SELECT username FROM admin WHERE status='Admin' AND id = $newid");
+                $row = mysqli_fetch_array($sql);
+                if($row != ''){
+                    mysqli_query($con,"INSERT INTO activitylogs (name,action,datemod,type,user)VALUES('$row[0]','Recover','$date','Admin','$user')");
+                }
+                mysqli_query($con,"UPDATE loginform SET deleted = 0 WHERE id = $newid");
+            }
+        }
+    }
 ?>
